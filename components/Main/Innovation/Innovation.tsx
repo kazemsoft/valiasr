08ad8bed9025deeb51ef5b579c0fab281b040d5c
matin <matinvilan1994@gmail.com>
2023-07-09 11:27:12 +0330

    improvement in website

--- components/Main/Innovation/Innovation.tsx
+++ components/Main/Innovation/Innovation.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Section from '../../global/Section/Section';
+import { Box } from '@chakra-ui/react';
+import List from './List';
 
 const Innovation = () => {
   return (
-    <div>
+    <Box marginTop={'100px'}>
       <Section
         title="Innovation and knowledge of society"
         description="Considering the current conditions of our society (high costs and corona conditions), by using some creativity and taste, we have prepared a creative product that will not only help you dear ones to electronicize your business, but will also significantly save your expenses. became."
       />
-    </div>
+      <List />
+    </Box>
   );
 };
 
---
+++ components/Main/Innovation/List.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { List as ChakraList, ListItem, ListIcon } from '@chakra-ui/react';
+import { CheckCircleIcon } from '@chakra-ui/icons';
+
+const List = () => {
+  return (
+    <ChakraList spacing={3}>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        Using the WordPress content management system
+      </ListItem>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        Popular WooCommerce store builder
+      </ListItem>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        PWA application powered by the popular React.js framework
+      </ListItem>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        Preparation and delivery of Android application (APK)
+      </ListItem>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        Integrity and non-interference of different parts due to 100% coding by
+        our own team.
+      </ListItem>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        SMS membership and login system
+      </ListItem>
+      <ListItem>
+        <ListIcon as={CheckCircleIcon} color="green.500" />
+        And ...
+      </ListItem>
+    </ChakraList>
+  );
+};
+
+export default List;
